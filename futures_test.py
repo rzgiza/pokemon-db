64e0543588f8b38b6f2a9e3340a8065b66dfe8b3
Robert Giza <rzgiza@gmail.com>
2024-04-09 00:48:22 -0700

    Morgify added to futures_test.py
    Morgify method used to insert data into js_pokemon table.

--- futures_test.py
+++ futures_test.py
@@ -1,3 +1,5 @@
+import psycopg2
+import hidden
 from concurrent.futures import as_completed
 from requests_futures.sessions import FuturesSession
 from pprint import pprint
@@ -14,7 +16,38 @@ with FuturesSession(max_workers=10) as session:
 	for future in as_completed(futures):
 		response = future.result()
 		pprint({
-			'pokemon': response.json()['name'],
 			'id': future.i
 		})
 		id_texts.append((future.i, response.text))
+
+# Load the secrets
+secrets = hidden.secrets()
+
+conn = psycopg2.connect(
+	host=secrets['host'],
+	port=secrets['port'],
+	database=secrets['database'],
+	user=secrets['user'],
+	password=secrets['pass'],
+	connect_timeout=3
+)
+cur = conn.cursor()
+
+sql = """
+CREATE TABLE IF NOT EXISTS js_pokemon (
+	id INTEGER, 
+	body JSONB
+);
+"""
+cur.execute(sql)
+print(sql)
+
+args = ','.join([cur.mogrify("(%s,%s)", tup).decode('utf-8') for tup in id_texts])
+
+cur.execute("INSERT INTO js_pokemon VALUES " + args)
+
+conn.commit()
+print("Values inserted into js_pokemon.")
+
+cur.close()
+conn.close()
