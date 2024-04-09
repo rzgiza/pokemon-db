97e845147b5da759c1217f5af2d447568db9baa1
Robert Giza <rzgiza@gmail.com>
2024-04-09 03:36:01 -0700

    Formatting spaces.

--- create_db.py
+++ create_db.py
@@ -10,12 +10,12 @@ from pprint import pprint
 # Load the secrets
 secrets = hidden.secrets()
 conn = psycopg2.connect(
-	host=secrets['host'],
-	port=secrets['port'],
-	database=secrets['database'],
-	user=secrets['user'],
-	password=secrets['pass'],
-	connect_timeout=3
+    host=secrets['host'],
+    port=secrets['port'],
+    database=secrets['database'],
+    user=secrets['user'],
+    password=secrets['pass'],
+    connect_timeout=3
 )
 cur = conn.cursor()
 print("Connection opened to PGSQL database.")
@@ -29,14 +29,14 @@ print(sql)
 
 sql = """
 CREATE TABLE IF NOT EXISTS js_pokemon (
-	id INTEGER, 
-	body JSONB
+    id INTEGER, 
+    body JSONB
 );
 
 CREATE TABLE IF NOT EXISTS pokedex (
-	id INTEGER PRIMARY KEY, name VARCHAR(20) UNIQUE, height NUMERIC, weight NUMERIC, hp NUMERIC,
-	attack NUMERIC, defense NUMERIC, s_attack NUMERIC, s_defense NUMERIC, speed NUMERIC, 
-	type TEXT [], info TEXT, evo_set INTEGER
+    id INTEGER PRIMARY KEY, name VARCHAR(20) UNIQUE, height NUMERIC, weight NUMERIC, hp NUMERIC,
+    attack NUMERIC, defense NUMERIC, s_attack NUMERIC, s_defense NUMERIC, speed NUMERIC, 
+    type TEXT [], info TEXT, evo_set INTEGER
 );
 """
 cur.execute(sql)
@@ -48,16 +48,16 @@ print("Results dumped into js_pokemon for further processing.")
 futures = []
 id_texts = []
 with FuturesSession(max_workers=10) as session:
-	for i in range(1, 1026):
-		url = 'https://pokeapi.co/api/v2/pokemon/' + str(i)
-		future = session.get(url)
-		future.i = i
-		futures.append(future)
+    for i in range(1, 1026):
+        url = 'https://pokeapi.co/api/v2/pokemon/' + str(i)
+        future = session.get(url)
+        future.i = i
+        futures.append(future)
 
-	for future in as_completed(futures):
-		response = future.result()
-		pprint({'pokemon_id': future.i})
-		id_texts.append((future.i, response.text))
+    for future in as_completed(futures):
+        response = future.result()
+        pprint({'pokemon_id': future.i})
+        id_texts.append((future.i, response.text))
 
 args = ','.join([cur.mogrify("(%s,%s)", tup).decode('utf-8') for tup in id_texts])
 cur.execute("INSERT INTO js_pokemon VALUES " + args)
@@ -68,19 +68,19 @@ print("Records inserted into js_pokemon: ", len(id_texts))
 
 sql = """
 INSERT INTO pokedex (
-	id, name, height, weight, hp, attack, defense, s_attack, s_defense, speed, type
+    id, name, height, weight, hp, attack, defense, s_attack, s_defense, speed, type
 )
 SELECT (body->'id')::int as id, 
-		body->'species'->>'name',
-	   (body->'height')::numeric,
-	   (body->'weight')::numeric, 
-	   (body->'stats'->0->'base_stat')::numeric,
-	   (body->'stats'->1->'base_stat')::numeric,
-	   (body->'stats'->2->'base_stat')::numeric,
-	   (body->'stats'->3->'base_stat')::numeric,
-	   (body->'stats'->4->'base_stat')::numeric,
-	   (body->'stats'->5->'base_stat')::numeric,
-	    translate(jsonb_path_query_array(body->'types', '$.type.name')::text, '[]', '{}')::text[]
+        body->'species'->>'name',
+       (body->'height')::numeric,
+       (body->'weight')::numeric, 
+       (body->'stats'->0->'base_stat')::numeric,
+       (body->'stats'->1->'base_stat')::numeric,
+       (body->'stats'->2->'base_stat')::numeric,
+       (body->'stats'->3->'base_stat')::numeric,
+       (body->'stats'->4->'base_stat')::numeric,
+       (body->'stats'->5->'base_stat')::numeric,
+        translate(jsonb_path_query_array(body->'types', '$.type.name')::text, '[]', '{}')::text[]
 FROM js_pokemon ORDER BY id ASC;
 """
 cur.execute(sql)
@@ -91,4 +91,3 @@ conn.commit()
 cur.close()
 conn.close()
 print("Connection closed to PGSQL database.")
-
